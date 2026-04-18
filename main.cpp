a98ada8f878b7294017f98d9118225d94ec4b666
anekobtw <elmars0608@gmail.com>
2026-04-18 17:32:15 -0400

    better filter top-level app windows

--- main.cpp
+++ main.cpp
@@ -1,33 +1,53 @@
+#include <iostream>
 #include <windows.h>
 #include <winuser.h>
 
-void CALLBACK WinEvenHook(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd,
-                          LONG idObject, LONG idChild, DWORD idEventThread,
-                          DWORD dwmsEventTime) {
+bool IsRealTopLevelAppWindow(HWND hwnd) {
+  if (!hwnd || !IsWindow(hwnd) || !IsWindowVisible(hwnd))
+    return false;
 
+  if (GetAncestor(hwnd, GA_ROOT) != hwnd)
+    return false;
+
+  if (GetWindow(hwnd, GW_OWNER) != NULL)
+    return false;
+
+  LONG_PTR style = GetWindowLongPtr(hwnd, GWL_STYLE);
+  LONG_PTR exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+
+  if (exStyle & WS_EX_TOOLWINDOW)
+    return false;
+
+  if (exStyle & WS_EX_NOACTIVATE)
+    return false;
+
+  return true;
+}
+
+void CALLBACK WinEventHookProc(HWINEVENTHOOK, DWORD event, HWND hwnd,
+                               LONG idObject, LONG idChild, DWORD, DWORD) {
   if (event != EVENT_OBJECT_SHOW)
     return;
+
   if (idObject != OBJID_WINDOW || idChild != CHILDID_SELF)
     return;
-  if (!IsWindowVisible(hwnd))
-    return;
-  if (GetAncestor(hwnd, GA_ROOT) != hwnd)
-    return;
-  if (GetWindow(hwnd, GW_OWNER) != NULL)
-    return;
-  if (!WS_EX_TOOLWINDOW)
+
+  if (!IsRealTopLevelAppWindow(hwnd))
     return;
 
-  MessageBoxA(NULL, "yesqaqa", "yes", MB_OK);
+  wchar_t title[256] = {0};
+  GetWindowTextW(hwnd, title, 256);
+
+  std::wcout << L"Window shown: hwnd=" << hwnd << L", title='" << title << L"'"
+             << std::endl;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, int nShowCmd) {
-  MessageBoxA(nullptr, "Hey!", "caption", MB_OK);
 
-  HWINEVENTHOOK hook =
-      SetWinEventHook(EVENT_OBJECT_SHOW, EVENT_OBJECT_SHOW, NULL, WinEvenHook,
-                      0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
+  HWINEVENTHOOK hook = SetWinEventHook(
+      EVENT_OBJECT_SHOW, EVENT_OBJECT_SHOW, NULL, WinEventHookProc, 0, 0,
+      WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
 
   if (!hook) {
     return 1;
@@ -39,5 +59,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     DispatchMessage(&msg);
   }
 
+  UnhookWinEvent(hook);
   return 0;
 }
